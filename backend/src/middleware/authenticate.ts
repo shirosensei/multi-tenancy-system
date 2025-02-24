18fabfacdd74b051ba1d27f2c4dee98710d59ab4
Shiro Sensei <codersensei@outlook.com>
2025-02-24 12:53:43 -0800

    fix(authenticate): fix payload to include token

--- backend/src/middleware/authenticate.ts
+++ backend/src/middleware/authenticate.ts
@@ -2,34 +2,45 @@ import { Request, Response, NextFunction } from "express";
 import { verifyToken } from "../utils/jwt";
 import logger from "../utils/logger";
 import { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
-import { TypedRequestBody, TypedRequestQuery, TypedRequestParams } from "../types/express";
+import { TypedRequestBody, TypedRequestQuery, TypedRequestParams, ApiResponse } from "../types/express";
 
-export const authenticate = <T>(
-  req: TypedRequestBody<T> | TypedRequestQuery<T> | TypedRequestParams<T>,
-  res: Response,
+export const authenticate = (
+  req: Request,
+  res: Response<ApiResponse<null>>,
   next: NextFunction
-) => {
+)  => {
   const authHeader = req.headers.authorization;
+
+  console.log('req.tenant:', req.tenant);
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     logger.warn("Unauthorized: Missing or invalid authorization header");
-    return res.status(401).json({ error: "Unauthorized" });
+     res.status(401).json({ success: false, error: "Unauthorized" });
+     return;
+     
   }
 
   const token = authHeader.split(" ")[1];
 
   if (!token) {
     logger.warn("Unauthorized: Missing token");
-    return res.status(401).json({ error: "Unauthorized" });
+     res.status(401).json({ success: false, error: "Unauthorized" });
+     return;
   }
 
   try {
     const decoded = verifyToken(token) as {
+      tenantId: string;
       name: string;
+      email: string; 
+      domain: string;      
       createdAt: Date;
       updatedAt: Date;
-      email: string; tenantId: string, domain: string;
     };
+
+    console.log('JWT Payload:', decoded);
+
+
     req.tenant = {
       domain: decoded.domain,
       id: decoded.tenantId,
@@ -39,21 +50,21 @@ export const authenticate = <T>(
       updatedAt: decoded.updatedAt
     }
 
-    // logger.info(
-    //   `User authenticated: userId=${decoded.userId}, tenantId=${decoded.tenantId}`
-    // );
+    logger.info(
+      `User authenticated: userId=${decoded.tenantId}, tenantId=${decoded.tenantId}`
+    );
 
     next();
   } catch (error) {
     if (error instanceof TokenExpiredError) {
       logger.error("Token expired", { error });
-      return res.status(401).json({ error: "Token expired" });
+       res.status(401).json({ success: false, error: "Token expired" });
     } else if (error instanceof JsonWebTokenError) {
       logger.error("Invalid token", { error });
-      return res.status(401).json({ error: "Invalid token" });
+       res.status(401).json({ success: false, error: "Invalid token" });
     } else {
       logger.error("Unathuorized", { error });
-      return res.status(401).json({ error: "Unauthorized" });
+       res.status(401).json({ success: false, error: "Unauthorized" });
     }
   }
 };

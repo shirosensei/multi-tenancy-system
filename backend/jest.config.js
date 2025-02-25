eeb76f399b46cf89efb037e6c04c92d473dcb2ca
Shiro Sensei <codersensei@outlook.com>
2025-02-24 22:36:43 -0800

    Deleted unwanted-file.txt

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest', // If you're using TypeScript
-  testEnvironment: 'node', // For Node.js environment
-  roots: ['<rootDir>/src'], // Look for tests in the src directory
-  testMatch: ['**/*.test.ts'], // Match test files ending with .test.ts
-  moduleFileExtensions: ['ts', 'js', 'json'], // File extensions to support
-  setupFilesAfterEnv: [], // Remove or update this line if not needed
-};
